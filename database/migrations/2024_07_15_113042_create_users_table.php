b9aef663e766d71bc6583149b60769ca050d5c58
Kengwabelinda <kengwawatkobelinda@gmail.com>
2024-07-18 18:25:22 +0200

    delet
    delete

--- database/migrations/2024_07_15_113042_create_users_table.php
+++ database/migrations/2024_07_15_113042_create_users_table.php
@@ -25,9 +25,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('status')->default('0');
             $table->unsignedBigInteger('role_id');
-            $table->foreign('role_id')->constrained()->references('id')->on('role')
-            ->onUpdate('cascade')
-            ->onDelete('cascade');
+          
 
             $table->timestamps();
 
