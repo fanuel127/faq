cda1d9996951747c1d032999ce75d1b3f7be5b11
Kengwabelinda <kengwawatkobelinda@gmail.com>
2024-07-29 19:03:53 +0200

    modifier une erreur
    erreur modifier

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\test;
-use Laravel\Ui\Presets\React;
+//use Laravel\Ui\Presets\React;
 
 class TestController extends Controller
 {
