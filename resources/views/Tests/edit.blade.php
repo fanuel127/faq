5c02a562727820116a0b7e6b9a1c03a764cbcbe9
Kengwabelinda <kengwawatkobelinda@gmail.com>
2024-08-21 08:43:10 +0100

    modify link
    modify link

--- resources/views/Tests/edit.blade.php
+++ resources/views/Tests/edit.blade.php
@@ -8,20 +8,11 @@
     <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.css">
     <script src="./bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
     <script src="./bootstrap-5.3.3-dist/js/bootstrap.js"></script>
-
-<<<<<<< Updated upstream
-=======
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-bs5/2.1.2/dataTables.bootstrap5.min.js"
-        integrity="sha512-Cwi0jz7fz7mrX990DlJ1+rmiH/D9/rjfOoEex8C9qrPRDDqwMPdWV7pJFKzhM10gAAPlufZcWhfMuPN699Ej0w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
->>>>>>> Stashed changes
+
     <title>edit test</title>
 </head>
 
